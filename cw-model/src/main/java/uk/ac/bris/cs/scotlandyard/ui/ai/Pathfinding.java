30e0d4a693cad8808f5c01de7732d25fda6d9fd8
Ethan <sk18181@bristol.ac.uk>
2019-04-29 19:42:02 +0100

    made change ai

---
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Pathfinding.java
@@ -0,0 +1,37 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+//import uk.ac.bris.cs.scotlandyard.model.Move;
+
+import uk.ac.bris.cs.scotlandyard.model.DoubleMove;
+import uk.ac.bris.cs.scotlandyard.model.TicketMove;
+import uk.ac.bris.cs.scotlandyard.model.PassMove;
+
+public class Pathfinding{
+
+    //calculates the minimum number of moves to get from one node to another
+    public int minJumps(int node1,int node2){
+        return 1;
+    }
+
+    //calculates the average number of hops it mr x is away from the detectives with the given move
+    public int averageMoveDist(TicketMove m){
+return 1;
+    }
+
+    public int averageMoveDist(DoubleMove m){
+        return 1;
+    }
+
+    public int averageMoveDist(PassMove m){
+
+
+        return Integer.MAX_VALUE;
+
+    }
+/*
+    public Move bestMove(Set<Move> moves){
+        
+    }
+*/
+
+}
