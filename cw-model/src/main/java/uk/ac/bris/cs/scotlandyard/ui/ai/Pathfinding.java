9c0d269952b1492405b884600a0bb9dac18c7c6d
Ethan <sk18181@bristol.ac.uk>
2019-04-30 23:00:41 +0100

    cleaned up

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Pathfinding.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Pathfinding.java
@@ -10,7 +10,6 @@ import uk.ac.bris.cs.scotlandyard.model.PassMove;
 import uk.ac.bris.cs.scotlandyard.model.Transport;
 import uk.ac.bris.cs.gamekit.graph.Edge;
 import uk.ac.bris.cs.gamekit.graph.Graph;
-import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
 import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYardModel;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYardPlayer;
@@ -22,30 +21,24 @@ public class Pathfinding{
 
         Graph<Integer,Transport> graph = model.getGraph();
         int[] nodeDistances=new int[graph.getNodes().size()+2];
-        //Boolean[] visited = new Boolean[graph.getNodes().size()];
 
         for(int i=1;i<graph.getNodes().size()+2;i++){
             nodeDistances[i]=Integer.MAX_VALUE;
         }
 
-        
         ArrayList<Node<Integer>> queue=new ArrayList<>();
         ArrayList<Node<Integer>> visited=new ArrayList<>();
         Node<Integer> startNode = graph.getNode(node1);
 
-        
         Node<Integer> endNode=graph.getNode(node2);
 
         nodeDistances[startNode.value()]=0;
         queue.add(startNode);
         while(!visited.contains(endNode)){
-            //System.out.println("visited size: "+String.valueOf(visited.size()));
-            //System.out.println("queue size: "+String.valueOf(queue.size()));
             
             Node<Integer> v=queue.get(0);
             queue.remove(0);
-            
-            //System.out.println("visited "+String.valueOf(v.value()));
+
             ArrayList<Edge<Integer,Transport>> edges =new ArrayList<Edge<Integer,Transport>>(graph.getEdgesFrom(v));
             for(Edge<Integer,Transport> item:edges){
                 if(!visited.contains(item.destination()) && !queue.contains(item.destination())){
@@ -56,13 +49,10 @@ public class Pathfinding{
             }
             visited.add(v);
         }
-
-        //System.out.println("mindistance"+String.valueOf(nodeDistances[endNode.value()]));
         return nodeDistances[endNode.value()];
-
     }
 
-    //calculates the average number of hops it mr x is away from the detectives with the given move
+    //calculates the average number of edges mr x is away from the detectives with after the move
     public static int averageMoveDist(int playerLocation,TicketMove m,ScotlandYardModel model){
         return minJumps(playerLocation, m.destination(),model);
     }
@@ -72,36 +62,33 @@ public class Pathfinding{
     }
 
     public static int averageMoveDist(int playerLocation,PassMove m,ScotlandYardModel model){
-
-
         return Integer.MIN_VALUE+1;
-
     }
+
+    //Finds the average distance away from the detectives the move produces
     public static int averageMoveDist(Move m,ScotlandYardModel model){
-        ArrayList<ScotlandYardPlayer> players=model.getMutablePlayers();
+        ArrayList<ScotlandYardPlayer> mutablePlayers=model.getMutablePlayers();
+        ArrayList<ScotlandYardPlayer> players = new ArrayList<>(mutablePlayers);
+        players.remove(0);
         int distanceSum=0;
         int location;
         for(ScotlandYardPlayer p:players){
             location=p.location();
-        if(m instanceof DoubleMove){
-            distanceSum+= averageMoveDist(location,(DoubleMove)m,model);
-        }
-
-        else if(m instanceof TicketMove){
-            distanceSum+= averageMoveDist(location,(TicketMove)m,model);
-        }
-        else if(m instanceof PassMove){
-            distanceSum+= averageMoveDist(location ,(PassMove)m,model);
-        }
         
-    }
-    
-    return distanceSum/players.size();
-}
+            if(m instanceof DoubleMove){
+                distanceSum+= averageMoveDist(location,(DoubleMove)m,model);
+            }
+            else if(m instanceof TicketMove){
+                distanceSum+= averageMoveDist(location,(TicketMove)m,model);
+            }
+            else if(m instanceof PassMove){
+                distanceSum+= averageMoveDist(location ,(PassMove)m,model);
+            }
+        }
+        return distanceSum/players.size();
+    }   
 
-    
-
-
+    //chooses the move with the highest score
     public static Move bestMove(Set<Move> moves,ScotlandYardModel model){
         int bestMoveScore=Integer.MIN_VALUE;
         Move bestMove=null;
@@ -113,6 +100,4 @@ public class Pathfinding{
         }
         return bestMove;
     }
-
-
 }
